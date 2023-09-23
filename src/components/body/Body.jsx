a1277b382558659059c0889daea4d55aafb131a4
Antje Stiller <antjestiller@gmx.de>
2023-09-23 17:31:07 +0200

    structur form element

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,22 +5,12 @@ export default function Body() {
 
   return (
     <main className='container'>
-      <div className="row">
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="text" className="form-control form-control-dark" placeholder="Search..." aria-label="Search" />
-          <input type="text" className="form-control form-control-dark" placeholder="Search..." aria-label="Search" />
-          <button type="button" className="btn btn-outline me-2">Login</button>
+      <div className="row mt-4">
+        <form className="row">
+            <input type="text" className="form-control form-control-dark col-6" placeholder="Top Text" aria-label="Search" />
+            <input type="text" className="form-control form-control-dark col-6" placeholder="Bottom Text" aria-label="Search" />
+            <button type="button" className="col-12 btn btn-dark my-3">Search</button>
         </form>
-
-        <div className="text-end">
-          <button type="button" className="btn btn-outlineme-2">Login</button>
-          <button type="button" className="btn btn-warning">Sign-up</button>
-        </div>
-      </div>
-
-      <div className="text-end">
-        <button type="button" className="btn btn-outline-light me-2">Login</button>
-        <button type="button" className="btn btn-warning">Sign-up</button>
       </div>
     </main>
   )
