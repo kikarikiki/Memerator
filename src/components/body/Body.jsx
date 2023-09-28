5539f3a0e20a799ee01dc80e66d67cdfe33ec77d
Antje Stiller <antjestiller@gmx.de>
2023-09-28 15:03:32 +0200

    adding text to memeImg

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './body.scss'
+import './meme.scss'
 import memesData from '../../memesData.js';
 
 export default function Body() {
@@ -11,27 +12,64 @@ export default function Body() {
   })
   const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
+
   function getMemeImage() {
     const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
     const url = memesArray[randomNumber].url
     setMeme(prevMeme => ({
-        ...prevMeme,
-        randomImage: url
+      ...prevMeme,
+      randomImage: url
     }))
-}
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    setMeme(prevMeme =>({
+      ...prevMeme,
+      [name]: value
+    }))
+  }
 
   return (
     <main className='container'>
       <div className="row mt-4">
         <form className="row">
-            <input type="text" className="form-control form-control-dark col-6" placeholder="Top Text" aria-label="Search" />
-            <input type="text" className="form-control form-control-dark col-6" placeholder="Bottom Text" aria-label="Search" />
-            <button type="button" className="col-12 btn btn-dark my-3" onClick={getMemeImage}>Search</button>
+          <div className="form-group col-6">
+            <input
+              type="text"
+              className="form-control form-control-dark col-6 mb-3"
+              placeholder="Top Text"
+              aria-label="Search"
+              name="topText"
+              value={meme.topText}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              className="form-control form-control-dark col-6"
+              placeholder="Bottom Text"
+              aria-label="Search"
+              name="bottomText"
+              value={meme.bottomText}
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className="col-12 btn btn-dark my-3"
+              onClick={getMemeImage}>Search
+            </button>
+          </div>
+          <div className="meme col-6">
+            <img
+              src={meme.randomImage}
+              className="img-fluid p-0"
+              alt=""
+            />
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+          </div>
         </form>
-        <div className="row">
-          <img src={meme.randomImage} className="p-0" alt="" />
-        </div>
       </div>
     </main>
   )
